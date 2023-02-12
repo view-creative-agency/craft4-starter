014cbd8d67ba43f168e8aca1d7fc24db2f4762f3
Matt Wilcox <mail@matthewwilcox.com>
2023-02-12 19:42:28 +0000

    CRON compatibility

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-echo -e "----\nbackup.sh called at $(date '+%Y-%m-%d %H:%M:%S')" >> script.log
+THIS_DIR="$(dirname "${BASH_SOURCE[0]}")" # Get the directory of the currently executing script
+CRON_LOG_PATH=/websites/_logs/cron.log
 
+# "cd" into this directory, because CRON runs from /home/staging and needs to move here
+cd ${THIS_DIR} || exit
+
+# Now do what we actually want to do...
+
+if [ $RUN_BY_CRON == true ]
+then
+	echo -e "----\n${THIS_DIR}/backup.sh called at $(date '+%Y-%m-%d %H:%M:%S') via CRON" >> ${CRON_LOG_PATH}
+fi
+
+echo -e "----\n${THIS_DIR}/backup.sh called at $(date '+%Y-%m-%d %H:%M:%S')" >> ${THIS_DIR}/script.log
 ./backup-files.sh
 ./backup-db.sh
