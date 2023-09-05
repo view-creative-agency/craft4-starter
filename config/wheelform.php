21c7043d36485a63f3d022d7e361afe8bca6858c
Matt Wilcox <mail@matthewwilcox.com>
2023-09-05 16:19:30 +0100

    Proper wheelform email notification template

---
+++ config/wheelform.php
@@ -0,0 +1,9 @@
+<?php
+// https://github.com/xpertbot/craft-wheelform/blob/master/README.md#custom-email-template
+
+return [
+	'template' => '_emails/wheelFormNotification',
+	'notification' => [
+		'template' => '_emails/wheelFormNotification',
+	],
+];
